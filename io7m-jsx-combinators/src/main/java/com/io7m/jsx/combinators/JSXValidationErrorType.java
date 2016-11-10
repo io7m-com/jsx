55e590cf8c3a5c21b2ec3316600cff7f6e684fa4
Mark Raynsford <code@io7m.com>
2016-11-10 17:24:07 +0000

    Add combinators for validating S-expressions.

---
+++ io7m-jsx-combinators/src/main/java/com/io7m/jsx/combinators/JSXValidationErrorType.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2016 <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.jsx.combinators;
+
+import com.io7m.jlexing.core.LexicalPositionType;
+import com.io7m.jsx.JSXImmutableStyleType;
+import org.immutables.value.Value;
+
+import java.nio.file.Path;
+
+/**
+ * An error produced during validation of an S-expression.
+ */
+
+@Value.Immutable
+@JSXImmutableStyleType
+public interface JSXValidationErrorType
+{
+  /**
+   * @return The lexical information
+   */
+
+  @Value.Parameter
+  LexicalPositionType<Path> lex();
+
+  /**
+   * @return The error message
+   */
+
+  @Value.Parameter
+  String message();
+}
